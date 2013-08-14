dc8b44cd2fb15d6fb256fda8ca970692f292cc69
IvÃan Mota <pinfake@hotmail.com>
2013-08-15 00:03:21 +0200

    Need to export CryptoJS in package.js as in meteor 0.6.5 this is mandatory

--- package.js
+++ package.js
@@ -1,7 +1,8 @@
 Package.describe({
-  summary: "Base Package for Crypto.js, standard secure algorithms"
+    summary: "Base Package for Crypto.js, standard secure algorithms"
 });
 
 Package.on_use(function (api) {
-  api.add_files('crypto.js', ['client', 'server']);
+    api.export( 'CryptoJS' );
+    api.add_files('crypto.js', ['client', 'server']);
 });
